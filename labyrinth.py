9ddf61922e4a69f789a6b616c7791b4d6e2ed39a
Kent Barbosa <kentbarbosa@gmail.com>
2016-03-27 17:26:47 -0400

    v0

--- labyrinth.py
+++ labyrinth.py
@@ -1,25 +1,45 @@
+import math
 import sys, pygame
 pygame.init()
 
-size = width, height = 320, 240
+size = width, height = 320, 320
 speed = [2, 2]
 black = 0, 0, 0
+white = 255, 255, 255
 
 screen = pygame.display.set_mode(size)
 
-ball = pygame.image.load("ball.gif")
-ballrect = ball.get_rect()
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+ringscale = 10.0
+x_offset = 200
+y_offset = 200
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+rings = []
+for ring in range(2,11):
+    if ring[rings] is None:
+        ring[rings] = []
+    lastpt = None
+    for radial in range(8):
+        angle = radial * math.pi/4
+        x = ring * ringscale * math.cos(angle) + x_offset
+        y = ring * ringscale * math.sin(angle) + y_offset
+        if lastpt :
+            rings[ring].append([lastpt,(x,y)])
+        lastpt = (x,y)        
+        print("{}, {}".format(x,y))
 
+print(rings)
+
+if 1:         
+##while 1:
+##    for event in pygame.event.get():
+##        if event.type == pygame.QUIT: sys.exit()
+##
     screen.fill(black)
-    screen.blit(ball, ballrect)
+    for ring in rings:
+        for lines in ring:
+            for line in lines:
+                print("draw line {}, {}".format(line[0],line[1]))
+                #pygame.draw.line(screen,white,int(line[0]),int(line[1]),2)
+
     pygame.display.flip()
 
